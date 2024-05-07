63d3bb0670d366dd229ba4227c9de73032a70ff3
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2024-05-07 16:23:33 -0300

    Update Hotel.java

--- sistema_hotel/src/br/com/sistema_hotel/hotel/Hotel.java
+++ sistema_hotel/src/br/com/sistema_hotel/hotel/Hotel.java
@@ -63,7 +63,35 @@ public synchronized boolean checkIn(Hospede hospede) {
         return checkInConcluido;
     }
 
+public synchronized boolean checkOut(Hospede hospede) {
+        System.out.println("Iniciando check-out para: " + hospede.getNome());
 
+        boolean checkOutConcluido = false;
+
+        for (Quarto quarto : quartos) {
+            if (quarto.getHospedes().contains(hospede)) {
+                quarto.removerHospede(hospede);
+                System.out.println("Check-out concluído para: " + hospede.getNome());
+                checkOutConcluido = true;
+                // Realocar hóspedes esperando
+                while (!filaEspera.isEmpty()) {
+                    Hospede proximoHospede = filaEspera.poll();
+                    if (checkIn(proximoHospede)) {
+                        System.out.println("Hóspede " + proximoHospede.getNome() + " realocado.");
+                    } else {
+                        break;
+                    }
+                }
+                break; // Sai do loop após realizar o check-out
+            }
+        }
+
+        if (!checkOutConcluido) {
+            System.out.println("Hóspede não encontrado para check-out: " + hospede.getNome());
+        }
+
+        return checkOutConcluido;
+    }
 
 
 
