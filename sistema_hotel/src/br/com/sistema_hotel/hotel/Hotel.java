6a22a3e17efc6ce232bd15254fbfbbda008b24dd
leocasagrande <leonardocasagrandesilva@hotmail.com>
2024-05-07 17:41:42 -0300

    check in e check out sendo feitos pelo Hotel

--- sistema_hotel/src/br/com/sistema_hotel/hotel/Hotel.java
+++ sistema_hotel/src/br/com/sistema_hotel/hotel/Hotel.java
@@ -49,6 +49,58 @@ private Quarto getQuartoDisponivel() {
         }
     }
 
+	public synchronized boolean checkIn(Hospede hospede) {
+	    int membrosRestantes = hospede.getMembrosFamilia();
+	    while(membrosRestantes > 0) {
+	        for (Quarto quarto : quartos) {
+	            if (quarto.isVago()) {
+	//                Se o quarto estiver vago, aloca um número máximo de 4 membros
+	//                (ou menos, se houver menos membros restantes) para esse quarto.
+	                int membrosAlocados = Math.min(membrosRestantes, 4);
+	                System.out.println("Membros alocados: " + membrosAlocados);
+	                quarto.adicionarHospede(hospede, membrosAlocados);
+	
+	                // System.out.println(quarto.getNumero());
+	                quarto.adicionarHospede(hospede, membrosAlocados);
+	                membrosRestantes -= membrosAlocados;
+	                if(membrosRestantes <= 0) {
+	                    hospedesAtivos.incrementAndGet(); // Incrementar o contador de hóspedes ativos
+	                    return true;
+	                }
+	            }
+	        }
+	        if (membrosRestantes > 0) {
+	            return false; // Há membros restantes, mas não há quartos suficientes disponíveis
+	        }
+	    }
+	    return true;
+	}
+	
+	 public synchronized void checkOut(Hospede hospede) {
+	        for (Quarto quarto : quartos) {
+	            if (quarto.getHospedes().contains(hospede)) {
+	                quarto.removerHospede(hospede);
+	                quarto.setChaveNaRecepcao(true); // Marcar a chave como na recepção para limpeza
+	                notifyAll(); // Notificar camareiras para limpeza do quarto
+	                break;
+	            }
+	        }
+	        if(hospedesAtivos.decrementAndGet() == 0) {
+	            System.out.println("Não há mais hospedes no hotel, portanto o sistema será encerrado");
+	            System.exit(0); // Método encerra a aplicação se não houver hóspedes ativos!
+	        }
+	    }
+	    
+	    public synchronized boolean temQuartoDisponivel() {
+	        for (Quarto quarto : quartos) {
+	            if (quarto.isVago()) {
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+
+
     public void solicitarCheckIn(Hospede hospede) throws InterruptedException {
         if (quartosOcupados.get() < quartos.size()) {
             filaCheckIn.offer(hospede);
