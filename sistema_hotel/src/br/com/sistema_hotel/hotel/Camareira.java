2131acf38aad0c3b357b446b0538d6689da028ae
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2024-05-07 16:05:00 -0300

    Update Camareira.java

--- sistema_hotel/src/br/com/sistema_hotel/hotel/Camareira.java
+++ sistema_hotel/src/br/com/sistema_hotel/hotel/Camareira.java
@@ -1,19 +1,36 @@
 package br.com.sistema_hotel.hotel;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 //
 // Source code recreated from a .class file by IntelliJ IDEA
 // (powered by FernFlower decompiler)
 //
 
 public class Camareira extends Thread {
-    public Hotel hotel;
+    private final Hotel hotel;;
 
-    public Camareira(Hotel var1) {
-        throw new Error("Unresolved compilation problems: \n\tThe declared package \"\" does not match the expected package \"br.com.atv2\"\n\tquartos cannot be resolved or is not a field\n\tThe method isChaveNaRecepcao() is undefined for the type Quarto\n\tThe method isVago() is undefined for the type Quarto\n\tThe method getNumero() is undefined for the type Quarto\n\tThe method getNumero() is undefined for the type Quarto\n\tThe method setChaveNaRecepcao(boolean) is undefined for the type Quarto\n\tThe method setChaveNaRecepcao(boolean) is undefined for the type Quarto\n");
+    public Camareira(Hotel hotel) {
+        this.hotel = hotel;
     }
 
     public void run() {
-        throw new Error("Unresolved compilation problem: \n");
+        while (true) {
+            Quarto quartoParaLimpar = null;
+            synchronized (hotel) {
+                while ((quartoParaLimpar = encontrarQuartoParaLimpar()) == null) {
+                    try {
+                        hotel.wait(); // Espera até ser notificado de que um quarto precisa de limpeza
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if (quartoParaLimpar != null) {
+                limparQuarto(quartoParaLimpar);
+            }
+        }
     }
 
     private Quarto encontrarQuartoParaLimpar() {
