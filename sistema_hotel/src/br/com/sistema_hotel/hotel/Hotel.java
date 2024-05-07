35004ff7a106448714fb0fca8b666d7d0305fa1e
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2024-05-07 16:14:29 -0300

    Update Hotel.java

--- sistema_hotel/src/br/com/sistema_hotel/hotel/Hotel.java
+++ sistema_hotel/src/br/com/sistema_hotel/hotel/Hotel.java
@@ -1,5 +1,10 @@
 package br.com.sistema_hotel.hotel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 //
 // Source code recreated from a .class file by IntelliJ IDEA
 // (powered by FernFlower decompiler)
