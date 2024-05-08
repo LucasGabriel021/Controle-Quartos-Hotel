94270de42da21ff45754e39846e261595289813f
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-05-07 21:01:13 -0300

    Alterando atributos e métodos de deixar chave

--- sistema_hotel/src/br/com/sistema_hotel/hotel/Quarto.java
+++ sistema_hotel/src/br/com/sistema_hotel/hotel/Quarto.java
@@ -1,66 +1,89 @@
 package br.com.sistema_hotel.hotel;
 
-import java.util.concurrent.locks.*;
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
 import java.util.ArrayList;
 import java.util.List;
 
 public class Quarto {
     private int numero;
-    private boolean vago;
     private List<Hospede> hospedes;
     private boolean chaveNaRecepcao;
+    private boolean vago; // Indica se o quarto esta vago
     private int ocupacaoAtual;
+    public static final int CAPACIDADE_MAXIMA = 4;
+    private boolean limpo = false; // Estado para rastrear se o quarto foi limpo
 
     public Quarto(int numero) {
         this.numero = numero;
-        this.vago = true;
         this.hospedes = new ArrayList<>();
         this.chaveNaRecepcao = true;
         this.ocupacaoAtual = 0;
-    }
-
-    public synchronized boolean isVago() {
-        return vago;
+        this.vago = true;  // Inicialmente, todos os quartos estão vazios
     }
 
     public synchronized List<Hospede> getHospedes() {
         return hospedes;
     }
 
-    public synchronized void adicionarHospede(Hospede hospede, int numeroDeMembros) {
-        if(ocupacaoAtual + numeroDeMembros <= 4) {
-            hospedes.add(hospede);
-            ocupacaoAtual += numeroDeMembros;
-            vago = false;
-        }
+    public synchronized boolean isVago() {
+        return ocupacaoAtual == 0;
     }
 
-    public synchronized void removerHospede(Hospede hospede) {
-        hospedes.remove(hospede);
-        if (hospedes.isEmpty()) {
-            vago = true;
-            chaveNaRecepcao = true;
+    public synchronized boolean adicionarHospede(Hospede hospede, int numeroDeMembros) {
+        if (ocupacaoAtual + numeroDeMembros <= CAPACIDADE_MAXIMA) {
+            hospedes.add(hospede);
+            ocupacaoAtual += numeroDeMembros;
+            setVago(false);
+            System.out.println(hospede.getNome() + " e seu grupo de " + hospede.getMembrosFamilia() + " pessoas " + " foram adicionados ao quarto de número: " + getNumero());
+            return true;
         }
+        System.out.println("Falha ao adicionar " + hospede.getNome() + " ao quarto de número " + getNumero() + " O quarto esta cheio ou não está vago!");
+        if(numeroDeMembros > CAPACIDADE_MAXIMA) {
+            System.out.println("Devido ao tamanho do grupo, serão alocados em mais de um quarto.");
+        }
+        return false;
+    }
+
+    public synchronized boolean removerHospede(Hospede hospede) {
+        if (hospedes.remove(hospede)) {
+            ocupacaoAtual -= 1; // Ajuste conforme a realidade de sua aplicação
+            System.out.println(hospede.getNome() + " e seu grupo de " + hospede.getMembrosFamilia()  + " pessoas foram removido(s) do quarto " + numero);
+            if (ocupacaoAtual == 0) {
+                chaveNaRecepcao = true;
+                setVago(true);  // O quarto está agora vago
+                System.out.println("O quarto " + numero + " agora está vago.");
+            }
+            return true;
+        }
+        return false;
     }
 
     public synchronized boolean isChaveNaRecepcao() {
         return chaveNaRecepcao;
     }
 
-    public synchronized void setChaveNaRecepcao(boolean chaveNaRecepcao) {
-        this.chaveNaRecepcao = chaveNaRecepcao;
+    public synchronized void deixarChaveNaRecepcao(String nome) {
+        this.chaveNaRecepcao = true;
+        System.out.println("Chave do quarto " + numero + " deixada na recepção por " + nome);
     }
 
-    public synchronized boolean isHospedesNoQuarto() {
-        return !hospedes.isEmpty();
+    public synchronized void pegarChaveDaRecepcao(String nome) {
+        this.chaveNaRecepcao = false;
+        System.out.println("Chave do quarto " + numero + " retirada da recepção por " + nome);
     }
 
     public int getNumero() {
-        // TODO Auto-generated method stub
-        return this.numero;
+        return numero;
+    }
+
+    public synchronized void setVago(boolean vago) {
+        this.vago = vago;
+    }
+
+    public boolean isLimpo() {
+        return limpo;
+    }
+
+    public void setLimpo(boolean limpo) {
+        this.limpo = limpo;
     }
 }
